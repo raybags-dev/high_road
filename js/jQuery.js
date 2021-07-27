bef553a689df8bfb35ae50ac0fac3265d2bc7f05
raybags <baguma.github@gmail.com>
2021-07-28 01:44:47 +0200

    intergrated back to top button

--- js/jQuery.js
+++ js/jQuery.js
@@ -16,5 +16,36 @@ $(function () {
     href: "https://raybags.com",
     target: "_blank",
   });
+
+  // Top icon
+
+  const div = $("<div></div>").attr("class", "to-top");
+  const top_icon = $("<i></i>").attr({
+    class: "fas fa-arrow-up to-top-arrow",
+  });
+  $(div).append(top_icon);
+  $("body").append(div);
+
+  // Show hide top btn handler
+  // hide show top button on scroll
+  function showHideTopBtn() {
+    if ($(window).scrollTop() > 200) {
+      $(".to-top").css({ opacity: 1 });
+    } else {
+      $(".to-top").css({ opacity: 0 });
+    }
+  }
+
+  // back to top on handler
+  // handle Smooth scrolling helper
+  const handlePageScroll = () => {
+    let target = $("body");
+    $("body, html").animate({ scrollTop: $(target).offset().top }, 500);
+    return false;
+  };
+
+  $(".to-top").on("click", handlePageScroll);
+
+  $(window).on("scroll", showHideTopBtn);
   // pageUnavailable();
 });
