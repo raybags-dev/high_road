b14564391ce84e25aa66577b045b87d8e6b14ac6
raybags <baguma.github@gmail.com>
2021-07-28 00:26:31 +0200

    impliment github and home components on card

--- js/jQuery.js
+++ js/jQuery.js
@@ -2,16 +2,19 @@
 
 import { pageUnavailable } from "./mentenance_page.js";
 
-$(document).ready(function () {
+$(function () {
   console.log("jQuery loaded");
 
   $(".tour-card").each((index, card) => {
     $(card)
       .delay(500 * index)
       .animate({
-        opacity: "1",
+        opacity: 1,
       });
   });
-  pageUnavailable();
-
+  $(".back-home-icon a").attr({
+    href: "https://raybags.com",
+    target: "_blank",
+  });
+  // pageUnavailable();
 });
--- js/mentenance_page.js
+++ js/mentenance_page.js
@@ -1,18 +1,29 @@
-"use strict"
-  // =====page mentenence code =====//
+"use strict";
+// =====page mentenence code =====//
 const pageUnavailable = () => {
-    const body = $("body");
-    const div = $("<div></div").attr("class", "mentenance-page-wrapper");
+  const body = $("body");
+  const div = $("<div></div").attr("class", "mentenance-page-wrapper");
 
-    const main_heading = $("<h1></h1").text("Projects").attr({class: "main-heading"});
-    const heading = $("<h2></h2").text("Page under maintenance").attr({class: "sub-heading"});
-    const paragraph = $("<p></p").text("The page will be ready soon. Please visit the page later").attr({class: "details"});
-    const link      = $("<a>").text("back home").attr({href: "https://raybags.com", target: "_blank", class: "back-home-link"})
+  const main_heading = $("<h1></h1")
+    .text("Projects")
+    .attr({ class: "main-heading" });
+  const heading = $("<h2></h2")
+    .text("Page under maintenance")
+    .attr({ class: "sub-heading" });
+  const paragraph = $("<p></p")
+    .text("The page will be ready soon. Please visit the page later")
+    .attr({ class: "details" });
+  const link = $("<a>")
+    .text("back home")
+    .attr({
+      href: "https://raybags.com",
+      target: "_blank",
+      class: "back-home-link",
+    });
 
-    div.append(main_heading, heading, paragraph, link);
-    body.append(div);
-    console.log(div);
-  };
-  // =====page mentenence code=====//
+  div.append(main_heading, heading, paragraph, link);
+  body.append(div);
+};
+// =====page mentenence code=====//
 
-  export { pageUnavailable };
+export { pageUnavailable };
