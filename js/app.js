2d112afa8424463fab150694f0717f71ea27595f
raybags <baguma.github@gmail.com>
2021-08-18 01:23:17 +0200

    implimented loading  skeleton and added random bacnkground images

--- js/app.js
+++ js/app.js
@@ -1,10 +1,41 @@
 "uset strict";
+const skelton = document.querySelector(".skeleton-container");
+const body = document.querySelector("body");
+// ********* nav ************
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+
+// remove skeleton after everything has loaded
+document.addEventListener("DOMContentLoaded", async (event) => {
+  if (event.bubbles == true)
+    setTimeout(() => skelton.classList.add("hide"), 2000);
+  // change background images every after 10minutes
+  const backgroundLinks = [
+    "../images/bg_imgs/bg1.jpeg",
+    "../images/bg_imgs/bg2.jpeg",
+    "../images/bg_imgs/bg6.jpeg",
+    "../images/bg_imgs/bg4.jpeg",
+  ];
+
+  let links = [],
+    index = 0;
+
+  links[0] = backgroundLinks[0];
+  links[1] = backgroundLinks[1];
+  links[2] = backgroundLinks[1];
+  links[3] = backgroundLinks[2];
+
+  setInterval(() => {
+    index = Math.floor(Math.random() * links.length);
+
+    document.body.style.cssText = `background: url(${backgroundLinks[index]})
+      center/cover no-repeat fixed;`;
+  }, 100000);
+});
+
 // ********* set date ************
 const date = (document.getElementById("date").innerHTML =
   new Date().getFullYear());
-// ********* nav toggle ************
-const navBtn = document.getElementById("nav-toggle");
-const links = document.getElementById("nav-links");
 
 // add event listener
 navBtn.addEventListener("click", () => {
